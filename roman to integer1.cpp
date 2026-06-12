f7867ca93171d4bf13fb294ac37eba747186694e
bambam29575 <123730907+bambam29575@users.noreply.github.com>
2026-06-12 16:22:52 +0530

    Create roman to integer1.cpp
    https://leetcode.com/problems/roman-to-integer?envType=study-plan-v2&envId=top-interview-150

---
+++ roman to integer1.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int size = s.size();
+        int result=0;
+        for(int i=0;i<size; i++)
+        {
+            if(s[i]=='I')
+            {
+                if(s[i+1]=='V')
+                {
+                    i++;
+                   result+=4;
+                }else if(s[i+1]=='X')
+                {
+                    i++;
+                    result += 9;
+                }else result += 1;
+            }
+
+            else if(s[i]=='X')
+            {
+                if(s[i+1]=='L')
+                {
+                    i++;
+                   result+=40;
+                }else if(s[i+1]=='C')
+                {
+                    i++;
+                    result += 90;
+                }else result += 10;
+            }
+            else if(s[i]=='C')
+            {
+                if(s[i+1]=='D')
+                {
+                    i++;
+                   result+=400;
+                }else if(s[i+1]=='M')
+                {
+                    i++;
+                    result += 900;
+                }else result += 100;
+            }
+            else if(s[i]=='V')
+            {
+                result+= 5;
+            }
+            else if(s[i]=='L')
+            {
+                result+= 50;
+            }
+            else if(s[i]=='D')
+            {
+                result+= 500;
+            }
+            else result += 1000;
+        }return result;
+    }
+};
